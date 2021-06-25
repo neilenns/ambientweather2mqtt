37f5707cfe6bac1ac6eb74b652a82f6f7ec63b3f
Neil Enns <info@danecreekphotography.com>
2021-06-25 14:01:11 -0700

    Express server set up, logging added

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,58 @@
  *  Copyright (c) Neil Enns. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-console.log("Hello world!");
+import * as webServer from "./webServer";
+import * as log from "./Log";
+
+/**
+ * Starts up the system.
+ */
+async function startup(): Promise<void> {
+  log.info("Main", "Starting up");
+
+  webServer.start();
+}
+
+/**
+ * Shuts down everything cleanly.
+ */
+async function shutdown(): Promise<void> {
+  await webServer.stop();
+}
+
+/**
+ * Called when a system shutdown message is received to ensure proper cleanup of open sockets.
+ */
+async function handleDeath(): Promise<void> {
+  log.info("Main", "Shutting down.");
+  await shutdown();
+  process.exit();
+}
+
+/**
+ * Registers for the various system shutdown messages to ensure proper cleanup.
+ */
+function registerForDeath(): void {
+  process.on("SIGINT", handleDeath);
+  process.on("SIGTERM", handleDeath);
+  process.on("SIGQUIT", handleDeath);
+  process.on("SIGBREAK", handleDeath);
+}
+
+/**
+ * The main method that runs the app.
+ */
+async function main(): Promise<void> {
+  registerForDeath();
+
+  await startup();
+
+  // Spin in circles waiting.
+  wait();
+}
+
+function wait() {
+  setTimeout(wait, 1000);
+}
+
+main();
