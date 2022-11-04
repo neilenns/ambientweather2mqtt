517dd0d9525b1adf474dac6fa8b98e88f3e70895
Nick Mowen <nickmowen213@gmail.com>
2022-11-04 07:06:23 -0600

    Add state class

---
+++ src/stateClass.ts
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Neil Enns. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+enum StateClass {
+  MEASUREMENT = "measurement"
+}
+
+export default StateClass;
