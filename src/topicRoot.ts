33cbd38969135cc2eae468a78c958e3d220665f7
Neil Enns <neile@live.com>
2023-07-08 16:34:55 -0700

    Provide an environment variable to set the topic root (#111)
    Fixes #110

---
+++ src/topicRoot.ts
@@ -0,0 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Neil Enns. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+const TopicRoot = process.env.TOPIC_ROOT || "homeassistant";
+
+export default TopicRoot;
