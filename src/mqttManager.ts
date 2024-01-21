2bc7c63a3f458d27d9ab33a89bd0c0c1dbd8fac8
Neil <neile@live.com>
2024-01-21 04:13:41 -0800

    Fix build break

--- src/mqttManager.ts
+++ src/mqttManager.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import MQTT from "async-mqtt";
+import * as MQTT from "async-mqtt";
 import * as log from "./log.js";
 import TopicRoot from "./topicRoot.js";
 
